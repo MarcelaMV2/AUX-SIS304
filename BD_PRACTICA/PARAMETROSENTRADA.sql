d79e36ea67a32b78784e64004b2f04bb279451a8
Marcela <mirandamarcela578@gmail.com>
2025-09-30 01:15:17 -0400

    parametros entrada

---
+++ BD_PRACTICA/PARAMETROSENTRADA.sql
@@ -0,0 +1,24 @@
+USE REPUESTOS;
+-- EJERCICIO1 PA que cuente la cantidad de repuestos eléctricos y mecánicos que se han comprado en una fecha específica.
+
+-- EJERCICIO2 PA que reciba como parámetro el nombre de un encargado y muestre todas las entregas que realizó, incluyendo el repuesto y la cantidad.
+
+-- EJERCICIO3 PA que reciba el código de un repuesto y el NIT del proveedor, y actualice el costo 
+-- de esa compra a un nuevo valor introducido por parámetro.
+
+-- EJERCICIO4 PA que reciba dos fechas como parámetros y muestre todas las compras registradas 
+-- dentro de ese rango, calculando para cada una de ellas el costo total real tomando en cuenta 
+-- el costo unitario, la cantidad adquirida, el descuento y el impuesto aplicados, de manera 
+-- que se refleje el valor final pagado en cada operación.
+
+-- EJERCICIO5 PA que reciba como parámetro un número mínimo y muestre únicamente a los 
+-- proveedores cuya cantidad de compras registradas supere ese valor, permitiendo identificar 
+-- a los que tienen mayor participación en las adquisiciones.
+
+-- EJERCICIO6 PA que inserte una compra cuando solo se conoce el nombre del repuesto, el nombre del proveedor,
+-- la cantidad y el precio unitario. Use variables locales.
+ 
+-- EJERCICIO7 PA que reciba el nombre de un proveedor y dos fechas, y calcule el total gastado en compras 
+-- dentro de ese rango. Use variables locales para almacenar el NIT.
+
+-- EJERCICIO8 PA que muestre los repuestos más comprados en un rango de fechas, y solo aquellos que superen una cantidad mínima.
